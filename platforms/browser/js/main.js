89cc3fa979b4335bbdcacccd7b6dce9bda5b74c7
unknown <bmcveigh@BMCVEIGH-F08402.cgifederal.com>
2016-04-22 12:56:48 -0400

    Putting js into it's own file.

---
+++ platforms/browser/js/main.js
@@ -0,0 +1,26 @@
+/**
+ * Created by bmcveigh on 4/22/2016.
+ */
+
+app.initialize();
+
+// Display JS errors.
+window.onerror = function(message, url, lineNumber) {
+    alert(("Error: "+message+" in "+url+" at line "+lineNumber));
+}
+
+var position = navigator.geolocation.getCurrentPosition(geolocationSuccess,
+    [geolocationError],
+    [geolocationOptions]);
+alert('it works');
+
+// $('body').prepend('Latitude: '          + position.coords.latitude          + '\n' +
+//   'Longitude: '         + position.coords.longitude         + '\n' +
+//   'Altitude: '          + position.coords.altitude          + '\n' +
+//   'Accuracy: '          + position.coords.accuracy          + '\n' +
+//   'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+//   'Heading: '           + position.coords.heading           + '\n' +
+//   'Speed: '             + position.coords.speed             + '\n' +
+//   'Timestamp: '         + position.timestamp                + '\n');
+// });
+$('body').prepend('Latitude: ' + position.coords.latitude + '\n');
