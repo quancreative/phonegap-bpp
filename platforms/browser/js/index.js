00d89dc6492e8fcdb3dbd789b68594fc169225ca
Quan Ngo <quan.ngo@cgifederal.com>
2016-04-22 15:39:37 -0400

    Added push notification plugin.

--- platforms/browser/js/index.js
+++ platforms/browser/js/index.js
@@ -34,6 +34,48 @@ var app = {
     // function, we must explicity call 'app.receivedEvent(...);'
     onDeviceReady: function() {
         app.receivedEvent('deviceready');
+
+        var push = PushNotification.init({
+            "android": {
+                "senderID": "1234567890"
+            },
+            "ios": {"alert": "true", "badge": "true", "sound": "true"},
+            "windows": {}
+        });
+
+        push.on('registration', function(data) {
+            console.log("registration event");
+            document.getElementById("regId").innerHTML = data.registrationId;
+            console.log(JSON.stringify(data));
+        });
+
+        push.on('notification', function(data) {
+            console.log("notification event");
+            console.log(JSON.stringify(data));
+            var cards = document.getElementById("cards");
+            var card = '<div class="row">' +
+                '<div class="col s12 m6">' +
+                '  <div class="card darken-1">' +
+                '    <div class="card-content black-text">' +
+                '      <span class="card-title black-text">' + data.title + '</span>' +
+                '      <p>' + data.message + '</p>' +
+                '    </div>' +
+                '  </div>' +
+                ' </div>' +
+                '</div>';
+            cards.innerHTML += card;
+
+            push.finish(function () {
+                $('.console-log').append("<div>finish successfully called!</div>");
+                console.log('finish successfully called');
+            });
+        });
+
+        push.on('error', function(e) {
+            $('.console-log').append("<div>Push error!</div>");
+            console.log("push error");
+        });
+
     },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
@@ -44,6 +86,40 @@ var app = {
         listeningElement.setAttribute('style', 'display:none;');
         receivedElement.setAttribute('style', 'display:block;');
 
-        console.log('Received Event: ' + id);
+        // each object in the array consists of a key which refers to the source and
+// the value which is the destination.
+        var filestocopy = [{
+            "resources/android/images/logo.png":
+                "platforms/android/res/drawable/logo.png"
+        }, {
+            "resources/android/sounds/ring.mp3":
+                "platforms/android/res/raw/ring.mp3"
+        }, {
+            "resources/ios/sounds/ring.caf":
+                "platforms/ios/YourAppName/ring.caf"
+        }, ];
+
+        var fs = require('fs');
+        var path = require('path');
+
+// no need to configure below
+        var rootdir = process.argv[2];
+
+        filestocopy.forEach(function(obj) {
+            Object.keys(obj).forEach(function(key) {
+                var val = obj[key];
+                var srcfile = path.join(rootdir, key);
+                var destfile = path.join(rootdir, val);
+                //console.log("copying "+srcfile+" to "+destfile);
+                var destdir = path.dirname(destfile);
+                if (fs.existsSync(srcfile) && fs.existsSync(destdir)) {
+                    fs.createReadStream(srcfile).pipe(
+                        fs.createWriteStream(destfile));
+                }
+            });
+        });
+
+
+
     }
 };
